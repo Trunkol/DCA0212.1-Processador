fa39d206906183fed5160e7db3684b90631f07fe
Emerson Costa <costtasemerson@gmail.com>
2018-11-29 11:33:36 -0300

    [ADD] Adicionando código para registrador de arquivos

--- dp.vhd
+++ dp.vhd
@@ -113,39 +113,43 @@ signal out0, out1, out2, out3 : std_logic_vector(3 downto 0);
 begin
 	process (rst, clk)
 	begin
-	
-	  -- take care of rst state
-	  
-	  if(clk'event and clk = '1')then
-		 if enb = '0' then
-			case (sel) is
-			  when "00" => 
-				 out0 <= input;
-			  when "01" => 
-				 -- insert proper statement here
-				 
-			  when "10" => 
-				 -- insert proper statement here
-				 
-			  when "11" =>
-				 -- insert proper statement here
-				 
-			  when others =>
-			end case;
-		 else
-			case (sel) is
-			  when "00" =>
-				 output <= out0;
-			  when "01" =>
-				 output <= out1;
-			  when "10" =>
-				 output <= out2;
-			  when "11" =>
-				 output <= out3;
-			  when others =>
-			end case;
-		 end if;
-	  end if;
+		-- take care of rst state
+		if (rst = '1') then
+			out0 <= "0000";
+			out1 <= "0000";
+			out2 <= "0000";
+			out3 <= "0000";
+		elsif(clk'event and clk = '1')then
+			case (atv) is
+				when "0100" =>
+					if enb = '0' then
+						case (sel) is
+							when "00" => 
+								out0 <= input;
+							when "01" =>
+								out1 <= input; 
+							when "10" => 
+								out2 <= input; 
+							when "11" =>
+								out3 <= input; 
+							when others =>
+						end case;
+					else
+						case (sel) is
+							when "00" =>
+								output <= out0;
+							when "01" =>
+								output <= out1;
+							when "10" =>
+								output <= out2;
+							when "11" =>
+								output <= out3;
+							when others =>
+						end case;
+					end if;
+				when others =>
+			end case;	
+		end if;
 	end process;	
 end bhv;
 
